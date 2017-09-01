eec685093997aee5b89f93708ff5ac2af24fc231
SAM-LEV-T410\awassell <awassell@fcptech.com>
2017-09-01 19:52:05 +0100

    git fuckd

--- script.js
+++ script.js
@@ -1,26 +1,6 @@
-angular.module('app', ['ngRoute', 'ngAnimate', 'ngTouch', 'ui.grid', 'ui.grid.pagination', 'ui.grid.resizeColumns', 'ui.grid.edit'])
-    .config(function($routeProvider){
-        $routeProvider
-        .when('/', {
-            templateUrl: 'pages/home.html',
-            controller: 'mainCtrl'
-        })
-    
-        .when('/#users', {
-            templateUrl: 'pages/users.html',
-            controller: 'usersCtrl'
-        })
-    
-        .when('/#logs', {
-            templateUrl: 'pages/logs.html',
-            controller: 'logsCtrl'
-        });
-    })
-    .controller('mainCtrl', ['$scope', function($scope){
-        $scope.message = 'Home message'
-    }])
-    .controller('usersCtrl', ['$scope', '$http', 'uiGridConstants', function ($scope, $http, uiGrigConstants) {
-        
+
+angular.module('app', ['ngAnimate', 'ngTouch', 'ui.grid', 'ui.grid.pagination', 'ui.grid.resizeColumns', 'ui.grid.edit'])
+    .controller('MainCtrl', ['$scope', '$http', 'uiGridConstants', function ($scope, $http, uiGrigConstants) {
         var paginationOptions = {
             pageNumber: 1,
             pageSize: 25,
